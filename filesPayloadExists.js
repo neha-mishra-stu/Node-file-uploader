dcb300e24ad21cc6facf21fc659d59a71fe9b655
Neha Mishra <nehamishra025479@gmail.com>
2025-04-14 00:23:49 +0530

    Delete filesPayloadExists.js

--- filesPayloadExists.js
+++
@@ -1,6 +0,0 @@
-const filesPayloadExists = (req, res, next) =>{
-    if(!res.files) return res.status(400).json({status: "error" , message: "missing files"})
-
-    next()    
-}
-module.exports = filesPayloadExists
